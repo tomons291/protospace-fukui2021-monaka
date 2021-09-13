376ba65becf80a158e58e95224d575aad5d80987
sasamon6005 <johnrock0618@gmail.com>
2021-09-13 12:10:01 +0900

    ユーザー登録と新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permited_parameters, if: :devise_controller?
 
   private
    
@@ -11,4 +11,9 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :affiliation, :position])
+  end
+
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,11 @@
 class PrototypesController < ApplicationController
+  #before_action :set_prototype, only: [:edit, :show]
+  #before_action :move_to_index, except: [:index, :show]
 
   def index
+    @prototypes = Prototype.all
   end
+
+  
   
 end
