3fdbe6eb104a244f6c44ae328e1f1755ab611893
kmsoku <sokuhuu5@gmail.com>
2021-09-15 19:07:23 +0900

    ユーザーデータ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @name=current_user.name
-    @prototype=current_user.prototypes
+    @user = User.find(params[:id])
+    
   end
+
 end
+
