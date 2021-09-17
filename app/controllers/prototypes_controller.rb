ff8ecab8b97115f5271b6b86edab18327d99ccc8
tomons291 <tomographycom@gmail.com>
2021-09-17 09:28:51 +0900

    プロトタイプ編集＿エラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -42,15 +42,21 @@ class PrototypesController < ApplicationController
 
   def update
     #️空白で登録するとエラーが出て、そのままの画面になる
-     prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to prototype_path(prototype.id)
+      @prototype = Prototype.find(params[:id])
+     
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+
     else
       render :edit
     end
   end
 
+  # def update
+  #    prototype = Prototype.find(params[:id])
+  #    prototype.update(prototype_params)
 
+  # end
 
   private
 
