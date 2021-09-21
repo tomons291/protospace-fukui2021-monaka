edd2654a49328cbe003e750275d4b8c98b1f92fe
kmsoku <sokuhuu5@gmail.com>
2021-09-21 14:13:27 +0900

    ユーザーのプロトタイプ情報とユーザーコントローラーのコード圧縮

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user =User.find(params[:id])
-    @prototype=@user.prototypes
+    @user = User.find(params[:id])
+    @prototype = @user.prototypes
   end
 end
